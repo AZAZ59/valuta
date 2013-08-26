d6971b52781f11a4e3a5a49d96b29ce733e660ad
AZAZ59 <stepsrg@email.ru>
2013-08-26 09:00:13 +0400

    удалён ненужный код

--- src/main/java/org/apache/myfaces/blank/Kurs.java
+++ src/main/java/org/apache/myfaces/blank/Kurs.java
@@ -4,18 +4,25 @@ public class Kurs {
 	String ticker;
 	float close;
 	int nominal;
-	public Kurs(String ticker,float close, int nominal) {
-		setValue(ticker,close, nominal);
+
+	public Kurs(String ticker, float close, int nominal) {
+		setValue(ticker, close, nominal);
 	}
+
 	public Kurs(String... a) {
 		// TODO Auto-generated constructor stub
-		setValue(a[0],Float.parseFloat(a[1]),Integer.parseInt(a[2]));
+		setValue(a[0], Float.parseFloat(a[1]), Integer.parseInt(a[2]));
 	}
 
-	private void setValue(String ticker,float close, int nominal) {
-		this.ticker=ticker;
-		this.close=close;
-		this.nominal=nominal;
+	private void setValue(String ticker, float close, int nominal) {
+		this.ticker = ticker;
+		this.close = close;
+		this.nominal = nominal;
 	}
 
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "\n"+ticker+" "+close+" "+nominal;
+	}
 }
--- src/main/java/org/apache/myfaces/blank/starter.java
+++ src/main/java/org/apache/myfaces/blank/starter.java
@@ -1,15 +1,15 @@
-
 package org.apache.myfaces.blank;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class starter {
 
@@ -17,52 +17,36 @@ public class starter {
 	 * @param args
 	 */
 
+	static final String asd = "http://export.rbc.ru/free/cb.0/free.fcgi?period=DAILY&tickers=NULL&d1=20&m1=08&y1=2013&d2=26&m2=08&y2=2013&lastdays=6&separator=%3B&data_format=BROWSER&header=0";
 
-	static final String asd ="http://export.rbc.ru/free/cb.0/free.fcgi?period=DAILY&tickers=NULL&d1=20&m1=08&y1=2013&d2=26&m2=08&y2=2013&lastdays=6&separator=%3B&data_format=BROWSER&header=1";
-	public static void main(final String[] args) throws IOException, ParseException {
+	public static void main(final String[] args) throws IOException,
+			ParseException {
 		// TODO Auto-generated method stub
 		URL con = new URL(asd);
 
 		HttpURLConnection http = (HttpURLConnection) con.openConnection();
 		http.setRequestMethod("GET");
-
 		Scanner s = new Scanner(http.getInputStream());
-		ArrayList<String> a = new ArrayList<>();
+
+		TreeMap<Date, ArrayList<Kurs>> storage = new TreeMap<>();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
 		while (s.hasNext()) {
-			a.add(s.nextLine());
-		}
+			String[] splStr = s.nextLine().split(";");
 
-		HashMap<Date, ArrayList<Kurs>> storage = new HashMap<>();
-
-
-		int sizeOfLine = a.get(0).split(";").length;
-		//String[][] table = new String[a.size() ][sizeOfLine];
-		for (int i = 1; i < a.size(); i++) {
-			String[] splStr = a.get(i).split(";");
-			System.out.println(Arrays.toString(splStr));
-
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			Date date = sdf.parse(splStr[1]);
-			if(!storage.containsKey(date)){
+			if (!storage.containsKey(date)) {
 				storage.put(date, new ArrayList<Kurs>());
 			}
 			storage.get(date).add(new Kurs(splStr[0], splStr[5], splStr[8]));
-
-			//table[i] = a.get(i).split(";");
-			//System.out.println(Arrays.toString(table[i]));
 		}
-
-		//print_table(table);
+		System.out.println(storage);
 
 	}
 
-	static void print_table(final String[][] table){
-		for(String[] s : table){
-			System.out.println(s[0]+
-					" "+s[1]+
-					" "+s[5]+
-					" "+s[8]
-					);
+	static void print_table(final String[][] table) {
+		for (String[] s : table) {
+			System.out.println(s[0] + " " + s[1] + " " + s[5] + " " + s[8]);
 		}
 	}
 
